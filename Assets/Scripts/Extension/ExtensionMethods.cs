1056252a3b616299be3d651befc342e954ca92f2
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-07-29 22:46:30 +0500

    add new Extention method

--- Assets/Scripts/Extension/ExtensionMethods.cs
+++ Assets/Scripts/Extension/ExtensionMethods.cs
@@ -30,4 +30,31 @@ public static class ExtensionMethods
 
         return dict;
     }
+
+    public static Dictionary<string, TValue> ToDictionary<TValue>(this IEnumerable<object> source)
+    {
+        Dictionary<string, TValue> dict = new Dictionary<string, TValue>();
+
+        foreach (object item in source)
+        {
+            FieldInfo[] fields = item.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+            if (fields.Length < 2)
+            {
+                throw new InvalidOperationException("The class must have at least two public fields.");
+            }
+
+            if (fields[0].FieldType == typeof(string))
+            {
+                string key = (string)fields[0].GetValue(item);
+                TValue value = (TValue)fields[1].GetValue(item);
+                dict.Add(key, value);
+            }
+            else
+            {
+                throw new InvalidOperationException("The first field must be of enum type.");
+            }
+        }
+
+        return dict;
+    }
 }
