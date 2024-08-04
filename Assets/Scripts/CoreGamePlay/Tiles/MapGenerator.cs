66f016d085bf32e46a4d2892bd5be89d847b4761
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-08-04 15:52:58 +0500

    add MapGenerator

---
+++ Assets/Scripts/CoreGamePlay/Tiles/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Helpers;
+using UnityEngine;
+
+namespace Assets.Scripts.CoreGamePlay
+{
+    public class MapGenerator : MonoBehaviour
+    {
+        [SerializeField, Range(1, 50)] private int _gridWidth = 16;
+        [SerializeField, Range(1, 50)] private int _gridDepth = 9;
+
+        [SerializeField] private Tile _tilePrefab;
+        [SerializeField] private Transform _parent;
+        [SerializeField] private float tileSpacing = 1.0f;
+
+        [ContextMenu("Generate")]
+        public void Generate()
+        {
+            for (int r = 0; r < _gridDepth; r++)
+            {
+                int rOffset = r / 2;
+                for (int q = -rOffset; q < _gridWidth - rOffset; q++)
+                {
+                    Tile tile = Instantiate(_tilePrefab, _parent);
+                    tile.transform.position = HexCalculator.ToWorldPosition(q, r, tileSpacing);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Helpers/HexCalculator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Helpers
+{
+    public static class HexCalculator
+    {
+        public static Vector3 ToWorldPosition(int q, int r, float tileSpacing)
+        {
+            float x = tileSpacing * Mathf.Sqrt(3) * (q + r / 2.0f);
+            float z = tileSpacing * 1.5f * r;
+            Vector3 position = new Vector3(x, 0, z);
+            return position;
+        }
+    }
+}
