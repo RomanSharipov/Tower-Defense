b6f238ca7eafbdf169d86dd25e31849cafc3534f
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-08-08 00:25:52 +0500

    fix tests TileInitializer

--- Assets/Scripts/CoreGamePlay/TileInitializer.cs
+++ Assets/Scripts/CoreGamePlay/TileInitializer.cs
@@ -15,28 +15,43 @@ namespace Assets.Scripts.CoreGamePlay
         
         [SerializeField] private Tile _start;
         [SerializeField] private Tile _target;
-        
+        [SerializeField] private Transform[] path;
+
+
         private void Awake()
         {
             foreach (Tile tile in _mapGenerator.GameBoardTiles)
             {
                 tile.InitializeNode(_mapGenerator.GameBoardTiles);
             }
+
+            foreach (Tile tile in _mapGenerator.GameBoardTiles)
+            {
+                tile.NodeBase.CacheNeighbors();
+            }
         }
 
         [ContextMenu("StartMovement()")]
         public void StartMovement()
         {
-            List<NodeBase> nodes = Pathfinding.FindPath(_start.NodeBase,_target.NodeBase);
-
-            Transform[] path = new Transform[nodes.Count];
+            List<NodeBase>  nodes = Pathfinding.FindPath(_start.NodeBase, _target.NodeBase);
+            
+            path = new Transform[nodes.Count];
 
             for (int i = 0; i < nodes.Count; i++)
             {
                 path[i] = nodes[i].Tile.transform;
             }
+
+            Array.Reverse(path);
             _enemy.Construct(path);
             _enemy.StartMovement();
+        }
+
+        [ContextMenu("SetStartPosition()")]
+        private void SetStartPosition()
+        {
+            _enemy.transform.position = _start.transform.position;
         }
     }
 }
