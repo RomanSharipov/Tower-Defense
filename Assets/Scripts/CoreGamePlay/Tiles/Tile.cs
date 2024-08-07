a25ea8e8017d2bfcc9393f2f8d351aa17f4ae233
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-08-07 22:28:45 +0500

    add Positions to Tile

--- Assets/Scripts/CoreGamePlay/Tiles/Tile.cs
+++ Assets/Scripts/CoreGamePlay/Tiles/Tile.cs
@@ -5,7 +5,16 @@ namespace Assets.Scripts.CoreGamePlay
     public class Tile : MonoBehaviour
     {
         [SerializeField] private TileId _tileId;
-
+        
         public TileId TileId => _tileId;
+
+        public int Q { get; private set; }
+        public int R { get; private set; }
+        
+        public void Construct(int q,int r)
+        {
+            Q = q;
+            R = r;
+        }
     }
 }
