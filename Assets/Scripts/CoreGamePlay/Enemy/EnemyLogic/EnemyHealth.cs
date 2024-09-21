eebdadddeb6b2db3689253e23077edbf3ecb1799
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-09-21 15:54:48 +0500

    fix enemy HealthBar

--- Assets/Scripts/CoreGamePlay/Enemy/EnemyLogic/EnemyHealth.cs
+++ Assets/Scripts/CoreGamePlay/Enemy/EnemyLogic/EnemyHealth.cs
@@ -1,7 +1,27 @@
-﻿namespace Assets.Scripts.CoreGamePlay
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.CoreGamePlay
 {
     public class EnemyHealth
     {
+        private readonly int _maxHealth;
+        private int _currentHealth;
 
+        public event Action<int,int> HealthChanged;
+
+        public EnemyHealth(int maxHealth)
+        {
+            _maxHealth = maxHealth;
+            _currentHealth = _maxHealth;
+        }
+
+        public void ReduceHealth(int value)
+        {
+            value = Mathf.Max(value, 0);
+            _currentHealth -= value;
+            _currentHealth = Mathf.Max(_currentHealth, 0);
+            HealthChanged?.Invoke(_currentHealth, _maxHealth);
+        }
     }
 }
---
+++ Assets/Scripts/Infrastructure/UI/RotationToCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RotationToCamera : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private bool _enabled;
+
+    public void Init(Camera camera)
+    {
+        _mainCamera = camera;
+        _enabled = true;
+    }
+
+    private void Update()
+    {
+        if (!_enabled)
+            return;
+
+        Quaternion rotation = _mainCamera.transform.rotation;
+        transform.LookAt(transform.position + rotation * Vector3.back, rotation * Vector3.up);
+    }
+}
