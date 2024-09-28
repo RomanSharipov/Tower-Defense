ac5eae037f9c0c4498e045e56f6c695320fdd5c8
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-09-28 22:05:25 +0500

    add CannonShell

---
+++ Assets/Scripts/CoreGamePlay/Turret/Projectiles/CannonShell.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CoreGamePlay
+{
+    public class CannonShell : MonoBehaviour
+    {
+        private int _damage;
+        private float _speed;
+
+        public void Init(int damage, float speed)
+        {
+            _damage = damage;
+            _speed = speed;
+        }
+
+        private void Update()
+        {
+            transform.position += transform.forward * _speed * Time.deltaTime;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out EnemyBase enemy))
+            {
+                enemy.TakeDamage(_damage);
+            }
+        }
+    }
+}
