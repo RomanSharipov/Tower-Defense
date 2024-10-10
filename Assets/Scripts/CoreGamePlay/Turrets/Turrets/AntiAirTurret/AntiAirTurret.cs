6984fd9c96d0ad77785a799583c7001241ec71db
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-10-10 23:55:36 +0500

    add base antiAirTurret

---
+++ Assets/Scripts/CoreGamePlay/Turrets/Turrets/AntiAirTurret/AntiAirTurret.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CoreGamePlay
+{
+    public class AntiAirTurret : TurretBase
+    {
+        [SerializeField] private AntiAirTurretAttack _antiAirTurretAttack;
+
+        public override AttackComponent AttackComponent => _antiAirTurretAttack;
+
+        public override void InitAttackComponent()
+        {
+            _antiAirTurretAttack.SetConfig(intervalBetweenAttack: 0.7f, damage: 30,bulletSpeed: 5f);
+        }
+    }
+}
---
+++ Assets/Scripts/CoreGamePlay/Turrets/Turrets/AntiAirTurret/AntiAirTurretAttack.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CoreGamePlay
+{
+    public class AntiAirTurretAttack : AttackComponent
+    {
+        [SerializeField] private CannonTurretAnimator[] _animators;
+        [SerializeField] private ParticleSystemCollection[] _effects;
+        
+        private CannonTurretAnimator _currentAnimator;
+        private ParticleSystemCollection _currentEffects;
+        private EnemyBase _enemyBase;
+
+        public override void Attack(EnemyBase enemyBase)
+        {
+            _currentAnimator.PlayAttack();
+            _enemyBase = enemyBase;
+        }
+
+        public override void SetLevel(int level)
+        {
+            _currentEffects = _effects[level];
+            _currentAnimator = _animators[level];
+        }
+
+        public void OnAnimationEventFire(int gunIndex)
+        {
+            _currentEffects.GetParticleSystemByIndex(gunIndex).Play();
+            _enemyBase.TakeDamage(_damage);
+        }
+
+        private void OnEnable()
+        {
+            foreach (CannonTurretAnimator animator in _animators)
+            {
+                animator.OnAnimationEventFire += OnAnimationEventFire;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (CannonTurretAnimator animator in _animators)
+            {
+                animator.OnAnimationEventFire -= OnAnimationEventFire;
+            }
+        }
+    }
+}
