845bd74732db7252456ab4b5ee66d759cf9ed399
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-10-19 20:22:35 +0500

    new slow turret

---
+++ Assets/Scripts/CoreGamePlay/Turrets/Turrets/SlowTurret2/SlowTurret2Attack.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CoreGamePlay
+{
+    public class SlowTurret2Attack : AttackComponent
+    {
+        [SerializeField] private ParticleSystemCollection[] _effects;
+
+        private ParticleSystemCollection _currentEffects;
+
+        public override void OnStartAttack(EnemyBase enemyBase)
+        {
+            base.OnStartAttack(enemyBase);
+            _currentEffects.Play();
+        }
+
+        public override void OnEndAttack()
+        {
+            base.OnEndAttack();
+            _currentEffects.Stop();
+        }
+
+        public override void Attack(EnemyBase enemyBase)
+        {
+            enemyBase.TakeDamage(_damage);
+        }
+
+        public override void SetLevel(int level)
+        {
+            _currentEffects = _effects[level];
+        }
+    }
+}
