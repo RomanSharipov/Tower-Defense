0be0bd154974a4bb1f99d76b2281d7af70b668cf
RomanSharipov <roman.sharipov.1990@gmail.com>
2024-10-20 16:11:21 +0500

    add FlameDamageTrigger to FlameTurret

--- Assets/Scripts/CoreGamePlay/Turrets/Turrets/FlameTurret/FlameTurretAttack.cs
+++ Assets/Scripts/CoreGamePlay/Turrets/Turrets/FlameTurret/FlameTurretAttack.cs
@@ -1,33 +1,79 @@
-﻿using UnityEngine;
+﻿using UniRx;
+using UnityEngine;
 
 namespace Assets.Scripts.CoreGamePlay
 {
     public class FlameTurretAttack : AttackComponent
     {
         [SerializeField] private ParticleSystemCollection[] _effects;
+        [SerializeField] private FlameDamageTriggerCollection[] _flameDamageTriggerCollections;
 
         private ParticleSystemCollection _currentEffects;
-
+        
+        private FlameDamageTriggerCollection _currrentFlameDamageTrigger;
+        private bool _nowAttack;
+        
         public override void OnStartAttack(EnemyBase enemyBase)
         {
+            _nowAttack = true;
             base.OnStartAttack(enemyBase);
             _currentEffects.Play();
+            _currrentFlameDamageTrigger.OnStartAttack();
         }
 
         public override void OnEndAttack()
         {
             base.OnEndAttack();
             _currentEffects.Stop();
+            _nowAttack = false;
+
+            Observable.Timer(System.TimeSpan.FromSeconds(1))
+                .Subscribe(_ =>
+                {
+                    if (!_nowAttack)
+                    {
+                        _currrentFlameDamageTrigger.ResetTrigger();
+                    }
+                })
+                .AddTo(this);
         }
 
         public override void Attack(EnemyBase enemyBase)
         {
-            enemyBase.TakeDamage(_damage);
+            _currrentFlameDamageTrigger.UpdateScaleAndPosition();
         }
 
         public override void SetLevel(int level)
         {
             _currentEffects = _effects[level];
+            _currrentFlameDamageTrigger = _flameDamageTriggerCollections[level];
+        }
+
+        private void OnEnemyEntered(EnemyBase enemy)
+        {
+            enemy.TakeDamage(_damage);
+        }
+
+        private void OnEnable()
+        {
+            foreach (FlameDamageTriggerCollection collections in _flameDamageTriggerCollections)
+            {
+                foreach (FlameDamageTrigger triger in collections.FlameDamageTriggers)
+                {
+                    triger.EnemyEntered += OnEnemyEntered;
+                }
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (FlameDamageTriggerCollection collections in _flameDamageTriggerCollections)
+            {
+                foreach (FlameDamageTrigger triger in collections.FlameDamageTriggers)
+                {
+                    triger.EnemyEntered -= OnEnemyEntered;
+                }
+            }
         }
     }
 }
